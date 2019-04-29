effa9d60706318e2d6937da062bd25fa6cae6bd8
ykwei0 <45205623+ykwei0@users.noreply.github.com>
2019-04-29 14:07:06 +0800

    Add files via upload

--- evil-extension/page.js
+++ evil-extension/page.js
@@ -12,9 +12,24 @@ const MATCH_LIST = {
 
 function transformTextNodes(node) {
   // TODO(you): Implement this function! See HW spec for details.
+  if(node.nodeType === Node.TEXT_NODE) {
+    //cut to words
+    let word = node.textContent.split(' ');
+    for (let i = 0; i < word.length; i++){
+      //delete WS and check if it is in MATCH_LIST
+      if (MATCH_LIST.hasOwnProperty(word[i].trim())){
+        word[i] = word[i].replace(word[i].trim(), MATCH_LIST[word[i].trim()]);
+      }
+    }
+    node.textContent = word.join(' ');
+  }
+  for (const child of node.childNodes) {
+    transformTextNodes(child);
+  }
 }
 
 transformTextNodes(document.body);
 
 // Log statement to test that the extension loaded properly.
 console.log('Evil extension loaded!');
+console.log('Extension updated');
